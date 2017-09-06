74425151a83ac3f7f6565e0e05c577dad046eeea
Amar Landa <AL@amarlanda.com>
2017-09-06 15:00:48 +0100

    first commit addes a GCI wrapper for Nayana

---
+++ GCI.ps1
@@ -0,0 +1,32 @@
+﻿# Powershell script to get the number of files greater than a given time period(in minutes) in the given share/directory and 
+# format the message in a way that Zabbix can interpret
+
+param (
+    [string]$path = $(write-host "ERROR: -path <path> is required"),
+    [Parameter(Mandatory=$false)][string]$duration,
+    [Parameter(Mandatory=$false)][string]$exclude,
+    [Parameter(Mandatory=$false)][string]$include
+)
+
+if(!($duration)){
+    $duration = 5
+}
+
+write-host $exclude + $include 
+
+$buildIncludeExcludecmd = ""
+if($exclude){
+    $buildIncludeExcludecmd = '-exclude ' + $exclude
+    write-host $buildIncludeExcludecmd
+}
+if($include){
+    $buildIncludeExcludecmd = $buildIncludeExcludecmd + ' -include ' + $include
+    write-host $buildIncludeExcludecmd
+}
+
+$val = get-childitem $path -recurse $buildIncludeExcludecmd
+write-host $val
+$files = get-childitem $path -recurse $buildIncludeExcludecmd | where {$_.LastWriteTime -lt (Get-Date).AddMinutes(-$duration)} | 
+
+Measure-Object
+return $files.count
---
+++ get-zabbixNasJob.ps1
@@ -0,0 +1,57 @@
+﻿   <#
+    .SYSNOPSIS
+    Wrapper for GCI, works by checking the duration of the last work write on the files being copied to the nas
+
+    .DESCRIPTION
+    if the file has more than 5 minutes on its last wirte time then the file has not been succesfully copied to the NAS server
+    
+    .NOTES
+    GCI Wrapper for Nayana
+    #>
+function Get-ZabbixNasJob
+{
+    [CmdletBinding()]
+ 
+    Param
+    (
+        # Madaory for Path, accepts pipeline 
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]
+        $path = 'C:\windows\help',
+
+        # Param2 help description
+        [string]
+        $exclude = "*.blabla",
+
+        [string]
+        $include = "*.ps1",
+
+        $duration = (Get-Date).addhours(-6)
+
+    )
+
+   
+    Process{
+
+        if(-not (test-path $path)){
+            write-warning "path $($path) is not accessible"
+        }else{
+            Write-Host "path $($path) is accessible :) " -ForegroundColor Green
+        }
+
+        #$date = (Get-Date).Addminutes(-5)
+        #$duration = (Get-Date).addhours(-6)
+
+        
+        $files = get-childitem -path $path -recurse -include "$include" -exclude "*.ni.*"| 
+        where-object {$_.LastWriteTimeUtc -gt $duration } | 
+        select fullname ,LastWriteTimeUtc
+        $files
+        
+    }
+    End
+    {
+    }
+}
